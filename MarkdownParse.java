f93b2245e45854c0a52e7a277556343e09a77429
Connor H Huang <cs15lwi22amb@ieng6-203.ucsd.edu>
2022-03-09 17:41:21 -0800

    final mdp

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,6 +13,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import java.util.*;
+
+class LinkCountVisitor extends AbstractVisitor {
+    ArrayList<String> links = new ArrayList<String>();
+    @Override
+    public void visit(Link link){
+        links.add(link.getDestination());
+    }
+}
 
 public class MarkdownParse {
     public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
@@ -30,113 +42,118 @@ public class MarkdownParse {
                 return result;
             }
             ArrayList<String> links = getLinks(Files.readString(p));
-            if(Files.readString(p).contains("[")&&Files.readString(p).contains("]")&&
-            Files.readString(p).contains("(")&&Files.readString(p).contains(")")){
-                result.put(dirOrFile.getPath(), links);
-            }
+            result.put(dirOrFile.getPath(), links);
+            
             return result;
         }
     }
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        ArrayList<Integer> openBracketIndexes = new ArrayList<>();
-        ArrayList<Integer> openParenIndexes = new ArrayList<>();
-        ArrayList<Integer> closeBracketIndexes = new ArrayList<>();
-        ArrayList<Integer> closeParenIndexes = new ArrayList<>();
-        //make and arraylist of all of the indexes of the occurances of the 
-        //different [, ], (, and )
+        Parser parser = Parser.builder().build();
+        Node node = parser.parse(markdown);
+        LinkCountVisitor visitor = new LinkCountVisitor();
+        node.accept(visitor);
+        toReturn.addAll(visitor.links);
 
-        //for the [
-        int currentIndex = 0;
-        while(currentIndex < markdown.lastIndexOf("[")) {
-            int openBracketIndex = markdown.indexOf("[", currentIndex);
-            openBracketIndexes.add(openBracketIndex);
-            currentIndex = openBracketIndex + 1;
-        }
-        //for ]
-        currentIndex = 0;
-        while(currentIndex < markdown.lastIndexOf("]")) {
-            int closeBracketIndex = markdown.indexOf("]", currentIndex);
-            closeBracketIndexes.add(closeBracketIndex);
-            currentIndex = closeBracketIndex + 1;
-        }
-        //for (
-        currentIndex = 0;
-        while(currentIndex < markdown.lastIndexOf("(")) {
-            int openParenIndex = markdown.indexOf("(", currentIndex);
-            openParenIndexes.add(openParenIndex);
-            currentIndex = openParenIndex + 1;
-        }
-        //for )
-        currentIndex = 0;
-        while(currentIndex < markdown.lastIndexOf(")")) {
-            int closeParenIndex = markdown.indexOf(")", currentIndex);
-            closeParenIndexes.add(closeParenIndex);
-            currentIndex = closeParenIndex + 1;
-        }
+        // ArrayList<String> toReturn = new ArrayList<>();
+        // ArrayList<Integer> openBracketIndexes = new ArrayList<>();
+        // ArrayList<Integer> openParenIndexes = new ArrayList<>();
+        // ArrayList<Integer> closeBracketIndexes = new ArrayList<>();
+        // ArrayList<Integer> closeParenIndexes = new ArrayList<>();
+        // //make and arraylist of all of the indexes of the occurances of the 
+        // //different [, ], (, and )
 
-        //checks the character before the open
-        for(int i = 0; i < openParenIndexes.size(); i++){
-            if(openParenIndexes.get(i) == 0){
-                openParenIndexes.remove(i);
-                i--;
-            }else if(markdown.charAt(openParenIndexes.get(i) - 1) != ']'){
-                openParenIndexes.remove(i);
-                i--;
-            }
-        }
+        // //for the [
+        // int currentIndex = 0;
+        // while(currentIndex < markdown.lastIndexOf("[")) {
+        //     int openBracketIndex = markdown.indexOf("[", currentIndex);
+        //     openBracketIndexes.add(openBracketIndex);
+        //     currentIndex = openBracketIndex + 1;
+        // }
+        // //for ]
+        // currentIndex = 0;
+        // while(currentIndex < markdown.lastIndexOf("]")) {
+        //     int closeBracketIndex = markdown.indexOf("]", currentIndex);
+        //     closeBracketIndexes.add(closeBracketIndex);
+        //     currentIndex = closeBracketIndex + 1;
+        // }
+        // //for (
+        // currentIndex = 0;
+        // while(currentIndex < markdown.lastIndexOf("(")) {
+        //     int openParenIndex = markdown.indexOf("(", currentIndex);
+        //     openParenIndexes.add(openParenIndex);
+        //     currentIndex = openParenIndex + 1;
+        // }
+        // //for )
+        // currentIndex = 0;
+        // while(currentIndex < markdown.lastIndexOf(")")) {
+        //     int closeParenIndex = markdown.indexOf(")", currentIndex);
+        //     closeParenIndexes.add(closeParenIndex);
+        //     currentIndex = closeParenIndex + 1;
+        // }
 
-        //does the image type check
-        for(int i = 0; i < openBracketIndexes.size(); i++){
-            if(openBracketIndexes.get(i) == 0){
-                openBracketIndexes.remove(i);
-                i--;
-            }else if(markdown.charAt(openBracketIndexes.get(i) - 1) == '!'){
-                openBracketIndexes.remove(i);
-                i--;
-            }
-        }
+        // //checks the character before the open
+        // for(int i = 0; i < openParenIndexes.size(); i++){
+        //     if(openParenIndexes.get(i) == 0){
+        //         openParenIndexes.remove(i);
+        //         i--;
+        //     }else if(markdown.charAt(openParenIndexes.get(i) - 1) != ']'){
+        //         openParenIndexes.remove(i);
+        //         i--;
+        //     }
+        // }
 
-        //does check for the closed brackets that must have a (, [, or : after for it to remain.
-        for(int i = 0; i < closeBracketIndexes.size(); i++){
-            if(closeBracketIndexes.get(i) == 0){
-                closeBracketIndexes.remove(i);
-                i--;
-            }else if(markdown.charAt(closeBracketIndexes.get(i) - 1) != '[' || 
-                markdown.charAt(closeBracketIndexes.get(i) - 1) != '(' ||
-                markdown.charAt(closeBracketIndexes.get(i) - 1) != ':'){
+        // //does the image type check
+        // for(int i = 0; i < openBracketIndexes.size(); i++){
+        //     if(openBracketIndexes.get(i) == 0){
+        //         openBracketIndexes.remove(i);
+        //         i--;
+        //     }else if(markdown.charAt(openBracketIndexes.get(i) - 1) == '!'){
+        //         openBracketIndexes.remove(i);
+        //         i--;
+        //     }
+        // }
 
-                closeBracketIndexes.remove(i);
-                i--;
-            }
-        }
+        // //does check for the closed brackets that must have a (, [, or : after for it to remain.
+        // for(int i = 0; i < closeBracketIndexes.size(); i++){
+        //     if(closeBracketIndexes.get(i) == 0){
+        //         closeBracketIndexes.remove(i);
+        //         i--;
+        //     }else if(markdown.charAt(closeBracketIndexes.get(i) - 1) != '[' || 
+        //         markdown.charAt(closeBracketIndexes.get(i) - 1) != '(' ||
+        //         markdown.charAt(closeBracketIndexes.get(i) - 1) != ':'){
+
+        //         closeBracketIndexes.remove(i);
+        //         i--;
+        //     }
+        // }
 
 
-        /**
-         * basically get an Integer Array of all of the indexes of [, ],(, and )
-         * to then compare the contents between each bracket and each parenthesis
-         * 
-         * Note:ignore check previous index if index == 0. if it's in the [ list then
-         *      skip that index and go to the next in the list elsewise remove the index
-         *      from the list
-         * 
-         * 1.   also remove all places where there is  \ character 
-         *      before the []() but if there is a second \\ then ignore.
-         * 2.   check all closed brackets to see if the next character is either (
-         *      or [ due to how markdown reads links as [text](link) or
-         *      [text][reference]
-         * 3.   if it follows the [text][reference] type, save the references into a
-         *      ArrayList<String> 
-         *              note: do the loops above to check if there's any "]: " to 
-         *                    see if it uses references and to mark where references could be
-         * 4.   create data class indexPair(int indexOpen, int indexClosed) and add all the
-         *      indexes of [ and ] that aren't disqualified previously
-         * 5.   disqualify pairs based on whether they contain
-         */
+        // /**
+        //  * basically get an Integer Array of all of the indexes of [, ],(, and )
+        //  * to then compare the contents between each bracket and each parenthesis
+        //  * 
+        //  * Note:ignore check previous index if index == 0. if it's in the [ list then
+        //  *      skip that index and go to the next in the list elsewise remove the index
+        //  *      from the list
+        //  * 
+        //  * 1.   also remove all places where there is  \ character 
+        //  *      before the []() but if there is a second \\ then ignore.
+        //  * 2.   check all closed brackets to see if the next character is either (
+        //  *      or [ due to how markdown reads links as [text](link) or
+        //  *      [text][reference]
+        //  * 3.   if it follows the [text][reference] type, save the references into a
+        //  *      ArrayList<String> 
+        //  *              note: do the loops above to check if there's any "]: " to 
+        //  *                    see if it uses references and to mark where references could be
+        //  * 4.   create data class indexPair(int indexOpen, int indexClosed) and add all the
+        //  *      indexes of [ and ] that aren't disqualified previously
+        //  * 5.   disqualify pairs based on whether they contain
+        //  */
 
-        for(int openParen:openParenIndexes){
-            toReturn.add(markdown.substring(openParen+1, markdown.indexOf(")",openParen)));
-        }
+        // for(int openParen:openParenIndexes){
+        //     toReturn.add(markdown.substring(openParen+1, markdown.indexOf(")",openParen)));
+        // }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
--- makefile
+++ makefile
@@ -1,22 +1,22 @@
 CLASSPATH = lib/*:.
 
 test:MarkdownParse.class MarkdownParseTest.java
-	javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
-	java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest	
+	javac -cp $(CLASSPATH) MarkdownParseTest.java
+	java -cp $(CLASSPATH) org.junit.runner.JUnitCore MarkdownParseTest	
 
 MarkdownParse.class:MarkdownParse.java
-	javac MarkdownParse.java
+	javac -cp $(CLASSPATH) MarkdownParse.java
 
 lab-report:MarkdownParse.class MarkdownParseLabTest.java
-	javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseLabTest.java
-	java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseLabTest	
+	javac -cp $(CLASSPATH) MarkdownParseLabTest.java
+	java -cp $(CLASSPATH) org.junit.runner.JUnitCore MarkdownParseLabTest	
 
 make:
-	javac MarkdownParse.java
-	java MarkdownParse test-files/
+	javac -cp $(CLASSPATH) MarkdownParse.java
+	java -cp $(CLASSPATH) MarkdownParse test-files/
 
 TryCommonMark.class:TryCommonMark.java
 	javac -g -cp $(CLASSPATH) TryCommonMark.java
 
 commonmark: TryCommonMark.class
-	
+
